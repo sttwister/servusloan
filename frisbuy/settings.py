af377d29b7dcf105de9acd4e16d799c08c05dcdf
Felix Kerekes <sttwister@gmail.com>
2012-09-07 18:27:43 +0300

    Added South and separate dev/prod settings

--- frisbuy/settings.py
+++ frisbuy/settings.py
@@ -12,17 +12,14 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -116,6 +113,7 @@ INSTALLED_APPS = (
 
     # Third-party apps
     'gunicorn',
+    'south',
 
     # Django apps
     'django.contrib.auth',
---
+++ frisbuy/settings_prod.py
@@ -0,0 +1,5 @@
+from frisbuy.settings import *
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
